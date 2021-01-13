a3e91f4bfae3e6f4c446e4cffcbf5522169e77f7
Wonro Park <park-7163@hanmail.net>
2021-01-14 03:35:53 +0900

    서비스 구현

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,8 +4,9 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
-
+// 서비스는 비지니스 로직 룰에 맞도록 네이밍해야한다.
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
@@ -16,13 +17,29 @@ public class MemberService {
     public Long join(Member member){
         //같은 이름이 있는 중복 회원X
 //        Optional<Member> result = memberRepository.findByName(member.getName()); //ctrl + art + v 단축키 Optional
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
 
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
 
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 검색
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
 }
