4f830ecb6a984c79da6e82d853fc5884903dc59f
Wonro Park <park-7163@hanmail.net>
2021-01-14 02:59:57 +0900

    회원 도메인 및 레포지토리 생성, Test

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,28 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+//        Optional<Member> result = memberRepository.findByName(member.getName()); //ctrl + art + v 단축키 Optional
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+}
